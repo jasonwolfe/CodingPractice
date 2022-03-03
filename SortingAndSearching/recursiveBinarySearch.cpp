9d51476df8941c8baeb62993aaba2aab3b0fd49f
jasonwolfe <j@sonwolfe.com>
2022-03-03 13:47:56 -0800

    minor correction to return value in recursion

--- SortingAndSearching/recursiveBinarySearch.cpp
+++ SortingAndSearching/recursiveBinarySearch.cpp
@@ -7,19 +7,21 @@ int recursiveBinarySearch(vector<int> sortedArray, int low, int high, int x){
         cerr << "ERROR: low is greater than high" << endl;
         return -1;
     }
+    int output = 0;
 
     int mid = low + (high - low)/2;
 
     if(sortedArray[mid]<x){
-        recursiveBinarySearch(sortedArray, mid+1, high, x);
+        mid = recursiveBinarySearch(sortedArray, mid+1, high, x);
     }else if(sortedArray[mid]>x){
-        recursiveBinarySearch(sortedArray, low, mid-1, x);
-    }else{
-        return mid;
+        mid = recursiveBinarySearch(sortedArray, low, mid-1, x);
     }
+    return mid;
 }
 
 int main(int argc, char* argv[]){
     vector<int> array = {1,2,3,4,5,6,7,8,9};
-    return recursiveBinarySearch(array, 0, array.size()-1, 3);
+    int returnValue = recursiveBinarySearch(array, 0, array.size()-1, 3);
+    cout << returnValue << endl;
+    return returnValue;
 }
