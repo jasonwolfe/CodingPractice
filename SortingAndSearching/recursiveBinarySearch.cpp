47346c71cee620cacd9c31b6327345a3bf88ec04
jasonwolfe <j@sonwolfe.com>
2022-03-03 12:13:54 -0800

    added recursive binary search example;

---
+++ SortingAndSearching/recursiveBinarySearch.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int recursiveBinarySearch(vector<int> sortedArray, int low, int high, int x){
+    if(low > high){
+        cerr << "ERROR: low is greater than high" << endl;
+        return -1;
+    }
+
+    int mid = low + (high - low)/2;
+
+    if(sortedArray[mid]<x){
+        recursiveBinarySearch(sortedArray, mid+1, high, x);
+    }else if(sortedArray[mid]>x){
+        recursiveBinarySearch(sortedArray, low, mid-1, x);
+    }else{
+        return mid;
+    }
+
+}
+
+int main(int argc, char* argv[]){
+    vector<int> array = {1,2,3,4,5,6,7,8,9};
+    return 0;
+}
