97149edd7832c2a2dc94b83df77341ce01da7e40
jasonwolfe <j@sonwolfe.com>
2022-07-27 21:26:20 -0700

    Javascript reverse-integer.js from leetcode

---
+++ Javascript/leetcode/reverse-integer.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let negative = false;
+    if(x<0){
+        negative = true;
+        x*=-1;
+    }
+    let value = 0;
+    while(x>0){
+        let temp = x%10;
+        value *= 10;
+        value += temp;
+        x = Math.trunc(x/10);
+        if(value>2147483647){
+            return 0;
+        }
+    }
+    if(negative){
+        value*=-1;
+    }
+    return value;
+};
+
+console.log(reverse(-3425));
